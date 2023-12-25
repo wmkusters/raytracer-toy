dc37139acdcca01f527a11ea64e5d6de88b18dc5
Bill Kusters <wmkusters@gmail.com>
2023-12-25 15:46:06 -0800

    wip hittablelist

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -1,10 +1,54 @@
 use std::fmt::{Display, Formatter, Result};
 use std::ops::{Add, Div, Mul, Neg, Sub};
 
+#[derive(Copy, Clone, Default)]
 struct HitRecord {
     p: Point,
-    normal: Vec,
+    normal: Vector,
     t: f64,
+    front_face: bool,
+}
+
+impl HitRecord {
+    fn set_face_normal(self, r: &Ray, outward_normal: Vector) {
+        self.front_face = r.direction.dot(outward_normal) < 0.0;
+        self.normal = if self.front_face {
+            outward_normal
+        } else {
+            -outward_normal
+        };
+    }
+}
+
+struct HittableList {
+    objects: Vec<Box<dyn Hittable>>,
+}
+
+impl Hittable for HittableList {
+    fn hit(self, r: &Ray, ray_tmin: f64, ray_tmax: f64, record: &mut HitRecord) -> bool {
+        let mut temp_rec: HitRecord = Default::default();
+        let hit_anything = false;
+        let closest_so_far = ray_tmax;
+
+        for obj in &self.objects {
+            if obj.hit(r, ray_tmin, closest_so_far, &mut temp_rec) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                record = &mut temp_rec;
+            }
+        }
+        hit_anything
+    }
+}
+
+impl HittableList {
+    fn add(self, item: Box<dyn Hittable>) {
+        self.objects.push(item);
+    }
+
+    fn clear(self) {
+        self.objects = Vec::new();
+    }
 }
 
 trait Hittable {
@@ -39,20 +83,21 @@ impl Hittable for Sphere {
 
         record.t = root;
         record.p = r.at(record.t);
-        record.normal = (record.p - self.center) / self.radius;
+        let outward_normal = (record.p - self.center) / self.radius;
+        record.set_face_normal(r, outward_normal);
 
         return true;
     }
 }
 
-#[derive(Copy, Clone)]
-struct Vec {
+#[derive(Copy, Clone, Default)]
+struct Vector {
     x: f64,
     y: f64,
     z: f64,
 }
 
-impl Display for Vec {
+impl Display for Vector {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         write!(f, "({}, {}, {})", self.x, self.y, self.z)
     }
@@ -60,10 +105,10 @@ impl Display for Vec {
 
 // don't need a second method for commutative addition,
 // commutation already works because we're adding floats
-impl Add<Vec> for Vec {
-    type Output = Vec;
-    fn add(self, rhs: Vec) -> Vec {
-        Vec {
+impl Add<Vector> for Vector {
+    type Output = Vector;
+    fn add(self, rhs: Vector) -> Vector {
+        Vector {
             x: self.x + rhs.x,
             y: self.y + rhs.y,
             z: self.z + rhs.z,
@@ -71,10 +116,10 @@ impl Add<Vec> for Vec {
     }
 }
 
-impl Sub<Vec> for Vec {
-    type Output = Vec;
-    fn sub(self, rhs: Vec) -> Self::Output {
-        Vec {
+impl Sub<Vector> for Vector {
+    type Output = Vector;
+    fn sub(self, rhs: Vector) -> Self::Output {
+        Vector {
             x: self.x - rhs.x,
             y: self.y - rhs.y,
             z: self.z - rhs.z,
@@ -82,10 +127,10 @@ impl Sub<Vec> for Vec {
     }
 }
 
-impl Neg for Vec {
-    type Output = Vec;
+impl Neg for Vector {
+    type Output = Vector;
     fn neg(self) -> Self::Output {
-        Vec {
+        Vector {
             x: -self.x,
             y: -self.y,
             z: -self.z,
@@ -93,10 +138,10 @@ impl Neg for Vec {
     }
 }
 
-impl Mul<f64> for Vec {
-    type Output = Vec;
+impl Mul<f64> for Vector {
+    type Output = Vector;
     fn mul(self, rhs: f64) -> Self::Output {
-        Vec {
+        Vector {
             x: self.x * rhs,
             y: self.y * rhs,
             z: self.z * rhs,
@@ -104,10 +149,10 @@ impl Mul<f64> for Vec {
     }
 }
 
-impl Div<f64> for Vec {
-    type Output = Vec;
-    fn div(self, rhs: f64) -> Vec {
-        Vec {
+impl Div<f64> for Vector {
+    type Output = Vector;
+    fn div(self, rhs: f64) -> Vector {
+        Vector {
             x: self.x / rhs,
             y: self.y / rhs,
             z: self.z / rhs,
@@ -126,7 +171,7 @@ impl Div<f64> for Vec {
 //    }
 //}
 
-impl Vec {
+impl Vector {
     fn length(self) -> f64 {
         self.length_squared().sqrt()
     }
@@ -139,12 +184,12 @@ impl Vec {
         self / self.length()
     }
 
-    fn dot(self, rhs: Vec) -> f64 {
+    fn dot(self, rhs: Vector) -> f64 {
         self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
     }
 
-    fn cross(self, rhs: Vec) -> Vec {
-        Vec {
+    fn cross(self, rhs: Vector) -> Vector {
+        Vector {
             x: self.y * rhs.z - self.z * rhs.y,
             y: self.z * rhs.x - self.x * rhs.z,
             z: self.x * rhs.y - self.y * rhs.x,
@@ -152,67 +197,34 @@ impl Vec {
     }
 }
 
-type Point = Vec;
+type Point = Vector;
 
 #[derive(Copy, Clone)]
 struct Ray {
     origin: Point,
-    direction: Vec,
+    direction: Vector,
 }
 
-fn hit_sphere(center: Point, radius: f64, r: &Ray) -> f64 {
-    let oc = r.origin - center;
-    let a = r.direction.length_squared();
-    let half_b = oc.dot(r.direction);
-    let c = oc.length_squared() - radius * radius;
-    let discriminant = half_b * half_b - a * c;
-
-    if discriminant < 0.0 {
-        return -1.0;
-    } else {
-        return (-half_b - discriminant.sqrt()) / a;
+fn ray_color(r: Ray, world: Box<dyn Hittable>) -> Vector {
+    let rec = HitRecord::default();
+    if world.hit(&r, 0.0, std::f64::INFINITY, &mut rec) {
+        return (rec.normal
+            + Vector {
+                x: 1.0,
+                y: 1.0,
+                z: 1.0,
+            })
+            * 0.5;
     }
-}
 
-fn ray_color(r: Ray) -> Vec {
-    let s = Sphere {
-        center: Vec {
-            x: 0.0,
-            y: 0.0,
-            z: -1.0,
-        },
-        radius: 0.5,
-    };
-    let mut rec = HitRecord {
-        p: Point {
-            x: 0.0,
-            y: 0.0,
-            z: 0.0,
-        },
-        normal: Vec {
-            x: 0.0,
-            y: 0.0,
-            z: 0.0,
-        },
-        t: 0.0,
-    };
-    s.hit(&r, -1.0, 2.0, &mut rec);
-    if rec.t > 0.0 {
-        let result = Vec {
-            x: rec.normal.x + 1.0,
-            y: rec.normal.y + 1.0,
-            z: rec.normal.z + 1.0,
-        } * 0.5;
-        return result;
-    }
     let unit_direction = r.direction.unit_vector();
     let a = (unit_direction.y + 1.0) * 0.5;
-    Vec {
+    Vector {
         x: 1.0,
         y: 1.0,
         z: 1.0,
     } * (1.0 - a)
-        + Vec {
+        + Vector {
             x: 0.5,
             y: 0.7,
             z: 1.0,
@@ -225,7 +237,7 @@ impl Ray {
     }
 }
 
-fn write_color(color: Vec) {
+fn write_color(color: Vector) {
     let output = color * 255.999;
     println!(
         "{0} {1} {2}",
@@ -238,6 +250,27 @@ fn main() {
     const IMAGE_WIDTH: u32 = 400;
     const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32;
 
+    let world = HittableList {
+        objects: Vec::new(),
+    };
+
+    world.add(Box::new(Sphere {
+        center: Point {
+            x: 0.0,
+            y: 0.0,
+            z: -1.0,
+        },
+        radius: 0.5,
+    }));
+    world.add(Box::new(Sphere {
+        center: Point {
+            x: 0.0,
+            y: -100.5,
+            z: -1.0,
+        },
+        radius: 100.0,
+    }));
+
     // camera
     const FOCAL_LENGTH: f64 = 1.0;
     const VIEWPORT_HEIGHT: f64 = 2.0;
@@ -249,12 +282,12 @@ fn main() {
     };
 
     // horiz/vert viewport edges
-    let viewport_u = Vec {
+    let viewport_u = Vector {
         x: VIEWPORT_WIDTH,
         y: 0.0,
         z: 0.0,
     };
-    let viewport_v = Vec {
+    let viewport_v = Vector {
         x: 0.0,
         y: -VIEWPORT_HEIGHT,
         z: 0.0,
@@ -266,7 +299,7 @@ fn main() {
 
     // upper left pixel
     let viewport_upper_left = camera_center
-        - Vec {
+        - Vector {
             x: 0.0,
             y: 0.0,
             z: FOCAL_LENGTH,
@@ -288,7 +321,7 @@ fn main() {
                 direction: ray_direction,
             };
 
-            let pixel_color = ray_color(r);
+            let pixel_color = ray_color(r, Box::new(world));
             write_color(pixel_color)
         }
     }
